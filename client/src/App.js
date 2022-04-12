c09a8f69c6dd8fb41ad73cf41e808451f891f0bb
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-04-12 15:31:30 -0600

    Added component directory and files

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import Navbar from "./components/Navbar";
+// import Project from "./components/Project";
+// import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <header className="header">
+            <h1>Samantha J Rexroat</h1>
+        </header>
+    );
+}
+
+export default Navbar;
