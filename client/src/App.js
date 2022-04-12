392e9abc485d14707cc164a25c35e244df853cf9
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-04-12 17:49:48 -0600

    Restructuring and added rendering logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,6 @@
-import React, {useState} from "react";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-// import Page from "./components/Page";
-// import Footer from "./components/Footer";
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  const [pages] = useState([
-    {name: "about"},
-    {name: "work"},
-    {name: "contact"},
-    {name: "resume"}
-  ]);
-
-  const [current, setCurrent] = useState(pages[0]);
-  return (
-    <>
-      <Header>
-      <Nav 
-        pages={pages}
-        setCurrent={setCurrent}
-        current={current}>
-      </Nav>
-      </Header>
-      {/* <main>
-        <Page current={current}></Page>
-      </main>
-      <Footer /> */}
-    </>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
