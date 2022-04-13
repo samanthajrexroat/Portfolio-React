3a5875a61a9485424ca9e5fd7464c7e0b612d731
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-04-12 18:07:00 -0600

    Added about link

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -8,14 +8,15 @@ function Nav({ currentPage, handlePageChange }) {
       href="#home"
       onClick={() => handlePageChange('Home')}
       >Samantha J. Rexroat</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         
         <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          <a 
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            >About</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Features</a>
