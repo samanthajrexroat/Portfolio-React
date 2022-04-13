d587e82c038512cb76bdb92718672a119faefea1
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-04-12 18:31:26 -0600

    Added screenshots of projects

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -3,15 +3,11 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h2>About</h2>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      A former educator with nine years of experience, I am an aspiring web developer looking for a position where I can best utilize my organizational skills, passion for growth and positive attitude. I believe that diligence and effective communication are critical to a prosperous work environment, and that positive relationships build strong teams. With previous experience across various fields, I have gained flexibility, knowledge and problem solving skills. As a vocalist and musician, I am committed to persistence and mastery. I look forward to utilizing these skills to help a company grow.
       </p>
+      <a href="">Resume</a>
     </div>
   );
 }
